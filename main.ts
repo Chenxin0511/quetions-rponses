c1639247923b9959f2235cf787857a37b4a012fe
Chenxin0511 <99085824+Chenxin0511@users.noreply.github.com>
2023-05-05 11:46:09 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,190 @@
+let question = 0
+input.onPinPressed(TouchPin.P0, function () {
+    if (question == 1) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 2) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 3) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 4) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 5) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 6) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 7) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 8) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 9) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+    if (question == 10) {
+        basic.clearScreen()
+        basic.showString("Bravo")
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (question == 1) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 2) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 3) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 4) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 5) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 6) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 7) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 8) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 9) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 10) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (question == 1) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 2) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 3) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 4) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 5) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 6) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 7) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 8) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 9) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+    if (question == 10) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    question = randint(1, 10)
+    if (question == 1) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(1)
+    }
+    if (question == 2) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(2)
+    }
+    if (question == 3) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(3)
+    }
+    if (question == 4) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(4)
+    }
+    if (question == 5) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(5)
+    }
+    if (question == 6) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(6)
+    }
+    if (question == 7) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(7)
+    }
+    if (question == 8) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(8)
+    }
+    if (question == 9) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(9)
+    }
+    if (question == 10) {
+        basic.clearScreen()
+        basic.showString("Question")
+        basic.showNumber(10)
+    }
+})
